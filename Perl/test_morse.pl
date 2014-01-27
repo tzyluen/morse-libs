872ed210b77918219c5825dc709d7928b4c4886c
Ng, Tzy Luen <tzyluen.ng@gmail.com>
2014-01-28 01:19:39 +0800

    added Perl version

---
+++ Perl/test_morse.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use Morse;
+
+my @morse_codes = [];
+while (my $line = <STDIN>) {
+    $line = uc($line);
+    my @chars = split(//, $line);
+    for my $c (@chars) {
+        my $morse_code = &get_morse($c);
+        if (defined $morse_code) {
+            print $morse_code;
+            push(@morse_codes, [$c, $morse_code]);
+        }
+    }
+} print "\n";
+
+
+for my $i (@morse_codes) {
+    if (defined $$i[0]) {
+        #print $$i[0];
+        print &to_letter($$i[1]);
+    }
+} print "\n";
